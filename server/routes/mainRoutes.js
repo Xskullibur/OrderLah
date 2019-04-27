1b23964f0772a16bbb90e04e6dce46a208b21e2b
Peh Zi Heng <pehziheng@gmail.com>
2019-04-27 11:16:05 +0800

    Added login page

--- server/routes/mainRoutes.js
+++ server/routes/mainRoutes.js
@@ -11,4 +11,11 @@ router.get('/', (req, res) => {
     res.render('index')
 })
 
+/**
+ * Login GET '/login' path
+ */
+router.get('/login', (req, res) => {
+    res.render('login', {layout: 'blank_layout'})
+})
+
 module.exports = router
