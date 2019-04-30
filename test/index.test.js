b016eccf4cd25922a316bc0c11cad61fefdc4f09
Peh Zi Heng <pehziheng@gmail.com>
2019-04-30 21:28:59 +0800

    Add testing framework

---
+++ test/index.test.js
@@ -0,0 +1,13 @@
+const expect = require('chai').expect
+
+//Database test suits
+
+describe('Users', () => {
+    it('Create user');
+
+    it('Read user');
+    
+    it('Update user');
+
+    it('Delete user');
+  });
