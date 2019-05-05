e1cc8540ca26a2f3ef80b641df786c3679219c0a
Peh Zi Heng <pehziheng@yahoo.com.sg>
2019-05-05 21:30:44 +0800

    Added sh script

---
+++ start-dev.sh
@@ -0,0 +1,10 @@
+
+
+#SET 'orderlah' database environment
+
+export DB_HOST="192.168.1.1"
+export DB_USERNAME="orderlah_web_user"
+export DB_PASSWORD="mysecurepassword"
+
+# start nodemon
+npm run start:dev
