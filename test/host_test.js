36b16f3fc9edb4a38a3c5038f8ae954df736abb5
Peh Zi Heng <pehziheng@gmail.com>
2019-05-11 20:09:11 +0800

    Host change

--- test/host_test.js
+++ test/host_test.js
@@ -1,5 +1,6 @@
 const process = require('process')
 
-const hostname = `http://${process.env.HOST}:${process.env.PORT}`
+// const hostname = `http://${process.env.HOST}:${process.env.PORT}`
+const hostname = `http://localhost:3000`
 
 module.exports = hostname
