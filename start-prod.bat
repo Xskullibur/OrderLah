d0f5dd5de6254c843688c02a0de01b1a5aec9c72
Peh Zi Heng <pehziheng@gmail.com>
2019-05-12 15:53:57 +0800

    Added menu item

---
+++ start-prod.bat
@@ -0,0 +1,15 @@
+:: Windows bat script
+
+@ECHO OFF
+
+:: SET 'orderlah' database environment
+
+SET DB_HOST=192.168.99.100
+SET DB_USERNAME=orderlah_web_user
+SET DB_PASSWORD=mysecurepassword
+
+:: start nodemon
+npm run start:dev
+
+
+PAUSE
