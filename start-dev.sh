e0da09967b5ed2177328934322ed449ffbb54128
Peh Zi Heng <pehziheng@yahoo.com.sg>
2019-05-15 14:49:59 +0800

    Fix background process not closing

--- start-dev.sh
+++ start-dev.sh
@@ -10,4 +10,10 @@ export DB_USERNAME="orderlah_web_user"
 export DB_PASSWORD="mysecurepassword"
 
 # start nodemon
-npm run start:dev
+npm run start:dev &
+
+trap "kill -9 $! && echo \"killed $!\" && exit 0" SIGINT SIGQUIT SIGTSTP SIGKILL
+while :
+do
+        sleep 60
+done
