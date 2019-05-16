cec0ce6467851c0ff74948db13b99673c6d34c5c
Peh Zi Heng <pehziheng@yahoo.com.sg>
2019-05-16 16:37:00 +0800

    Fix login width size

--- start-prod.sh
+++ start-prod.sh
@@ -5,15 +5,9 @@
 # set Production
 export NODE_ENV="prod"
 
-export DB_HOST="192.168.99.100"
+export DB_HOST="localhost"
 export DB_USERNAME="orderlah_web_user"
 export DB_PASSWORD="mysecurepassword"
 
 # start nodemon
 npm run start
-
-trap "kill -9 $! && echo \"killed $!\" && exit 0" SIGINT SIGQUIT SIGTSTP SIGKILL
-while :
-do
-        sleep 60	
-done
