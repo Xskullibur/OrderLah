866d60acd5fceb6fc40b879cbe4c9b37efe3863f
Xskullibur <simasas2222@gmail.com>
2019-05-19 22:15:33 +0800

    added image for MenuItem

--- server/models/menuItem.js
+++ server/models/menuItem.js
@@ -3,6 +3,7 @@ module.exports = {
         return function (Sequelize, db) {
 
             const MenuItem = db.define('menuItem', {
+                image: { type: Sequelize.STRING, allowNulls: true, unique: false },
                 itemName: { type: Sequelize.STRING(50), allowNulls: false, unique: false },
                 itemDesc: { type: Sequelize.STRING(255), allowNulls: false, unique: false },
                 price: { type: Sequelize.DECIMAL(10, 2), allowNulls: false, unique: false },
