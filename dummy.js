5fe39a9b13ef7413a9b39de3057825bc697413ad
Xskullibur <simasas2222@gmail.com>
2019-05-19 17:25:10 +0800

    Updated DB Schema and Added Data

--- dummy.js
+++ dummy.js
@@ -8,26 +8,13 @@ const MenuItem  = globalHandle.get('menuItem')
 
 module.exports = () => {
 
-    //Create test user
-    User.create({
-        username: 'John',
-        firstName: 'john',
-        email: 'Hancock@test',
-        birthday: new Date('11/2/2018'),
-        password: 'test',
-        phone: '1231231',
-        role: 'Customer',
-    }).then(john => {
-        console.log("John's auto-generated ID:", john.id);
-    }).catch(err => console.error(err))
-
     //Create Stall and Owner
     User.create({
-        username: 'Nosla',
+        username: 'Stall Owner',
         firstName: 'nosla',
-        email: 'nosla@chickenrice',
+        email: 'nosla@stallowner',
         birthday: new Date('2000/09/16'),
-        password: 'tuturu~',
+        password: 'test',
         phone: '91234567',
         role: 'Admin',
 
@@ -35,6 +22,7 @@ module.exports = () => {
 
         console.info('\nNosla (Stall Owner):', stallOwner.dataValues);
 
+        //Chicken Rice
         Stall.create({
             stallName: 'Nosla\'s Chicken Rice',
             cusine: 'Asian',
@@ -42,6 +30,119 @@ module.exports = () => {
             userId: stallOwner.id,
         }).then(stall => {
             console.info('\nNosla\'s Chicken Rice Stall:', stall.dataValues);
+            MenuItem.create({
+                itemName: 'Chicken Rice',
+                itemDesc: 'A dish of poached chicken and seasoned rice, served with chili sauce and cucumber garnishes',
+                price: 2.50,
+                active: true,
+                stallId: stall.id
+            }).then(item => {
+                User.create({
+                    username: 'Customer',
+                    firstName: 'customer',
+                    email: 'customer@test',
+                    birthday: new Date('11/2/2018'),
+                    password: 'test',
+                    phone: '1231231',
+                    role: 'Customer',
+                }).then(customer => {
+                    console.log("John's auto-generated ID:", customer.id);
+
+                    //Pending Order
+                    Order.create({
+                        status: 'Order Pending',
+                        orderTiming: new Date,
+                        userId: customer.id,
+                        stallId: stall.id
+                    }).then(order => {
+                        OrderItem.create({
+                            quantity: 2,
+                            orderId: order.id,
+                            menuItemId: item.id
+                        })
+                    })
+
+                    //Completed Order
+                    Order.create({
+                        status: 'Collection Confirmed',
+                        orderTiming: new Date,
+                        userId: customer.id,
+                        stallId: stall.id
+                    }).then(order => {
+                        OrderItem.create({
+                            quantity: 5,
+                            rating: "5",
+                            comments: "The chicken rice was FANTASTIC!!!",
+                            orderId: order.id,
+                            menuItemId: item.id
+                        })
+                    })
+                }).catch(err => console.error(err))
+            })
+
+            MenuItem.create({
+                itemName: 'Duck Rice',
+                itemDesc: 'Braised Duck with plain rice',
+                price: 3.50,
+                active: true,
+                stallId: stall.id
+            })
+
+            MenuItem.create({
+                itemName: 'Roasted Pork Rice',
+                itemDesc: 'The chunks of lightly crispy, springy, and well-seasoned roast pork with rice',
+                price: 3.30,
+                active: true,
+                stallId: stall.id
+            })
+
+            MenuItem.create({
+                itemName: 'Deleted Rice',
+                itemDesc: 'Used to test deleted menu items',
+                price: 99.99,
+                active: false,
+                stallId: stall.id
+            })
+        }).catch(err => console.error(err))
+
+        //Western
+        Stall.create({
+            stallName: 'Nayrb\'s Western',
+            cusine: 'Western',
+            description: 'Most delicious western food at NYP!',
+            userId: stallOwner.id,
+        }).then(stall => {
+            console.info('\nNayrb\'s Western:', stall.dataValues);
+        }).catch(err => console.error(err))
+
+        //Japanese
+        Stall.create({
+            stallName: 'Tuturu\'s Donburi',
+            cusine: 'Japanese',
+            description: 'Tuturu\'s Donburi e yokoso~',
+            userId: stallOwner.id,
+        }).then(stall => {
+            console.info('\nTuturu\'s Donburi:', stall.dataValues);
+        }).catch(err => console.error(err))
+
+        //Noodle Stall
+        Stall.create({
+            stallName: 'Eldoon\'s Noodle House',
+            cusine: 'Asian',
+            description: 'Enjoy the best handmade noodles here today!',
+            userId: stallOwner.id,
+        }).then(stall => {
+            console.info('\nEldoon\'s Noodle House:', stall.dataValues);
+        }).catch(err => console.error(err))
+
+        //Drink
+        Stall.create({
+            stallName: 'Genh Ihz\'s Refereshments',
+            cusine: 'Mixed',
+            description: 'Thirsty? Here you go...',
+            userId: stallOwner.id,
+        }).then(stall => {
+            console.info('\nHenh Ihz\'s Refreshments', stall.dataValues);
         }).catch(err => console.error(err))
 
     }).catch(err => console.error(err))
--- server/models/menuItem.js
+++ server/models/menuItem.js
@@ -6,6 +6,7 @@ module.exports = {
                 itemName: { type: Sequelize.STRING(50), allowNulls: false, unique: false },
                 itemDesc: { type: Sequelize.STRING(255), allowNulls: false, unique: false },
                 price: { type: Sequelize.DECIMAL(10, 2), allowNulls: false, unique: false },
+                active: { type: Sequelize.BOOLEAN, allowNulls: true, unique: false},
             })
 
             return MenuItem;
