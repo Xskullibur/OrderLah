8d7c6757a843aec65f15339f4ab2472048cacf2d
Xskullibur <simasas2222@gmail.com>
2019-05-26 14:23:32 +0800

    currentOrders color changes

--- server/routes/stallOwnerRoutes.js
+++ server/routes/stallOwnerRoutes.js
@@ -107,4 +107,74 @@ router.get('/currentOrders', (req, res, next) => {
 
 });
 
+router.get('/allOrders', (req, res, next) => {
+
+    Order.findAll({
+        where: {
+            status: {
+                [Sequelize.Op.or]: ['Order Pending', 'Preparing Order', 'Ready For Collection',]
+            }
+        },
+        order: Sequelize.col('orderTiming'),
+        include: [{
+            model: MenuItem
+        }]
+
+    }).then((currentOrders) => {
+        // res.send(currentOrders);
+
+        const testImg = process.cwd() + '/public/img/no-image'
+        res.render('../views/stallOwner/currentOrders', {
+            helpers: {
+                calcTotal(order){
+                    let sum = 0;
+                    order.menuItems.forEach(order => {
+                        sum += order.price*order.orderItem.quantity
+                    });
+                    return sum.toFixed(2);
+                },
+                calcItemPrice(items){
+                    return (items.price * items.orderItem.quantity).toFixed(2)
+                },
+                formatDate(date, formatType){
+                    return moment(date).format(formatType);
+                },
+                getTitle(menuItem){
+                    let title = []
+
+                    menuItem.forEach(item => {
+                        title.push(`${item.itemName} x${item.orderItem.quantity}`)
+                    });
+
+                    return title.join(', ')
+                },
+                getNextStatus(status){
+                    let updatedStatus = "";
+                    switch (status) {
+                        case 'Order Pending':
+                            updatedStatus = "Preparing Order"
+                            break;
+                    
+                        case 'Preparing Order':
+                            updatedStatus = "Ready for Collection"
+                            break;
+
+                        case 'Ready for Collection':
+                            updatedStatus = "Collection Confirmed"
+                            break;
+
+                        default:
+                            break;
+                    }
+
+                    return updatedStatus;
+                }
+            },
+            currentOrders
+        });
+
+    }).catch((err) => console.error(err));
+
+});
+
 module.exports = router;
