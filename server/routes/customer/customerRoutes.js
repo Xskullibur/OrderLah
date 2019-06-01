2e722c519b0a8c69fa28f0b7479c9084b6d31ee9
Bryan <182869N@mymail.nyp.edu.sg>
2019-06-01 20:48:19 +0800

    redid pastorders.handlebar (will be redone again, this is a temporary fix)

--- server/routes/customer/customerRoutes.js
+++ server/routes/customer/customerRoutes.js
@@ -1,9 +1,16 @@
 const express = require('express')
 //Create router
 const router = express.Router()
+//MomentJS
+const moment = require('moment')
 
 //Global
 const globalHandle = require('../../libs/global/global')
+const Order = globalHandle.get('order');
+const MenuItem = globalHandle.get('menuItem');
+
+//Sequelize
+const Sequelize = require('sequelize')
 
 //Setup uuid for csrf authentication
 const uuid_middleware = require('../../libs/uuid_middleware')
@@ -27,10 +34,77 @@ router.get('/review', (req, res) => {
 });
 
 router.get('/pastOrders', (req, res) => {
-    res.render('customer/pastorders',{})
-});
+    Order.findAll({
+        where: {
+            status: {
+                [Sequelize.Op.or]: ['Order Pending', 'Preparing Order', 'Ready For Collection',]
+            },
+            // stallId: req.user.id
+        },
+        order: Sequelize.col('orderTiming'),
+        include: [{
+            model: MenuItem
+        }]
 
-router.get('/orderStatus', (req, res) => {
+    }).then((currentOrders) => {
+        // res.send(currentOrders);
+
+        const testImg = process.cwd() + '/public/img/no-image'
+        res.render('customer/pastorders', {
+            helpers: {
+                calcTotal(order){
+                    let sum = 0;
+                    order.menuItems.forEach(order => {
+                        sum += order.price*order.orderItem.quantity
+                    });
+                    return sum.toFixed(2);
+                },
+                calcItemPrice(items){
+                    return (items.price * items.orderItem.quantity).toFixed(2)
+                },
+                formatDate(date, formatType){
+                    return moment(date).format(formatType);
+                },
+                getTitle(menuItem){
+                    let title = []
+
+                    menuItem.forEach(item => {
+                        title.push(`${item.itemName} x${item.orderItem.quantity}`)
+                    });
+
+                    return title.join(', ')
+                },
+                getNextStatus(status){
+                    let updatedStatus = "";
+                    switch (status) {
+                        case 'Order Pending':
+                            updatedStatus = "Preparing Order"
+                            break;
+                    
+                        case 'Preparing Order':
+                            updatedStatus = "Ready for Collection"
+                            break;
+
+                        case 'Ready for Collection':
+                            updatedStatus = "Collection Confirmed"
+                            break;
+
+                        default:
+                            break;
+                    }
+
+                    return updatedStatus;
+                }
+            },
+            currentOrders
+        });
+
+    }).catch((err) => console.error(err));
+
+});/*res.render('customer/pastorders',{})
+});*/
+
+router.get('/trackOrder', (req, res) => {
     res.render('customer/orderStatus',{})
 });
 
