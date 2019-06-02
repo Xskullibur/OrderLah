fa798ff64246aba22fe92516e363694c7d20fcd5
lee hsien xiang <leehsienx@gmail.com>
2019-06-02 16:47:45 +0800

    added script for menu items

---
+++ Script/menu.sql
@@ -0,0 +1,10 @@
+INSERT INTO `orderlah_db`.`menuItems` (`id`, `image`, `itemName`, `itemDesc`, `price`, `active`, `owner`) 
+VALUES ('1', NULL, 'chicken rice', 'OMG tasty', '2.00', '1', '4'),
+('2', NULL, 'taco', 'wow just wow', '4.00', '1', '2'),
+('3', NULL, 'steak', 'tasty', '6.00', '1', '2'),
+('4', NULL, 'ice cream', 'wow just wow', '2.00', '1', '2'),
+('5', NULL, 'lobster', 'good', '24.00', '1', '4'),
+('6', NULL, 'curry mee', 'greaT', '5.00', '1', '4');
+
+
+
