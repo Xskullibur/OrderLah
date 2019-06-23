02c468bf2aafde0a95ba88caa27aa5a6503f36e9
Xskullibur <simasas2222@gmail.com>
2019-06-23 23:30:29 +0800

    Stall Owner Info added to pdf

--- public/js/PDFmake.js
+++ public/js/PDFmake.js
@@ -1,10 +1,8 @@
 var pdfMake = require('pdfmake/build/pdfmake.js');
 var pdfFonts = require('pdfmake/build/vfs_fonts.js');
-var imgFile = require('../img/logo.png')
 pdfMake.vfs = pdfFonts.pdfMake.vfs;
 
 // Functions
-
 function calcDailyTotal(orders) {
   let sum = 0
 
@@ -35,21 +33,19 @@ function calcOrderTotal(order) {
   return sum.toFixed(2);
 }
 
-function getDataUri(url, callback) {
-  var image = new Image();
+function getDataURL(url) {
 
-  image.onload = function () {
-      var canvas = document.createElement('canvas');
-      canvas.width = this.naturalWidth; // or 'width' if you want a special/scaled size
-      canvas.height = this.naturalHeight; // or 'height' if you want a special/scaled size
+    var img = new Image()
+    img.src = url
 
-      canvas.getContext('2d').drawImage(this, 0, 0);
+    var canvas = document.createElement('canvas');
+        canvas.width = img.width;
+        canvas.height = img.height;
 
-      // ... or get as Data URI
-      callback(canvas.toDataURL('image/png'));
-  };
+    var ctx = canvas.getContext('2d');
+    ctx.drawImage( img, 0, 0 ); 
+    return canvas.toDataURL( );
 
-  image.src = url;
 }
 
 // Get orderInfo
@@ -113,78 +109,102 @@ function orderInfo(formatedOrder) {
   return content
 }
 
-function showPDF(formatedOrder, summaryDate, mode) {
+function showPDF(formatedOrder, summaryDate, stallOwner , mode) {
 
-    getDataUri('../../img/logo.png', function(dataUri) {
+    let docDefinition = {
+      
+        // PDF File Info 
+        info: {
+          title: `${summaryDate} Order Summary`,
+          author: 'john doe',
+          subject: 'subject of document',
+        },
 
-      let docDefinition = {
-        
-          info: {
-            title: `${summaryDate} Order Summary`,
-            author: 'john doe',
-            subject: 'subject of document',
+        // PDF Body
+        content: [
+          
+          {
+            columns: [
+              {width: '*', text: ''},
+              {
+                layout: {defaultBorder: false, paddingLeft: function(i, node) { return 10 }, paddingRight: function(i, node) { return 0 }, paddingTop: function(i, node) { return 0 }, paddingBottom: function(i, node) { return 0 }},
+                width: 'auto',
+                table: {
+                  headerRows: 1,
+                  height: ['auto', 'auto'],
+                  width: ['*', '*'],
+                  body: [
+                    ['Stall:', `${stallOwner.stall.stallName}`],
+                    ['User:', `${stallOwner.firstName}`],
+                    ['Email:', `${stallOwner.email}`],
+                    ['Tel:', `${stallOwner.phone}`]
+                  ]
+                }
+              },
+            ]
           },
 
-          content: [
+          {
+            image: getDataURL('../../img/logo.png'),
+            fit: [100, 100],
+            alignment: 'center'
+          },
 
-            {
-              image: dataUri,
-              fit: [100, 100],
-              alignment: 'center'
-            },
+          // Title
+          {
+            text: `${summaryDate} Order Summary`,
+            style: 'header',
+            alignment: 'center'
+          },
 
-            // Title
-            {
-              text: `${summaryDate} Order Summary`,
-              style: 'header',
-              alignment: 'center'
-            },
-  
-            orderInfo(formatedOrder),
-  
-          ],
-  
-          styles: {
-            header: {
-              fontSize: 18,
-              margin: [0, 0, 0, 30],
-              bold: true
-            },
-            subheader: {
-              fontSize: 15,
-              bold: true
-            },
-            quote: {
-              italics: true
-            },
-            small: {
-              fontSize: 8
-            },
-            normalTable: {
-              margin: [0, 0, 0, 30]
-            }
+          orderInfo(formatedOrder),
+
+        ],
+
+        // Footer
+        footer: {
+          columns: [{ text: `Generated At: ${Date()}`, alignment: 'right', margin: [0, 0, 50, 0] }]
+        },
+
+        styles: {
+          header: {
+            fontSize: 18,
+            margin: [0, 0, 0, 30],
+            bold: true
+          },
+          subheader: {
+            fontSize: 15,
+            bold: true
+          },
+          quote: {
+            italics: true
+          },
+          small: {
+            fontSize: 8
+          },
+          normalTable: {
+            margin: [0, 0, 0, 30]
           }
-  
-          // header: function(currentPage, pageCount, pageSize) {
-          //   // you can apply any logic and return any valid pdfmake element
-        
-          //   return [
-          //     { text: `${summaryDate}`, alignment: (currentPage % 2) ? 'left' : 'right' },
-          //     { canvas: [ { type: 'rect', x: 170, y: 32, w: pageSize.width - 170, h: 40 } ] }
-          //   ]
-          // },
-          // footer: function(currentPage, pageCount) { return currentPage.toString() + ' of ' + pageCount; }
-      }
-  
-      console.log(docDefinition);
-  
-      if (mode === 'show') {
-        pdfMake.createPdf(docDefinition).open({}, window.open('', '_blank') );
-      }
-      else if (mode == 'print') {
-        pdfMake.createPdf(docDefinition).print();
-      }
+        }
 
-    })
+        // header: function(currentPage, pageCount, pageSize) {
+        //   // you can apply any logic and return any valid pdfmake element
+      
+        //   return [
+        //     { text: `${summaryDate}`, alignment: (currentPage % 2) ? 'left' : 'right' },
+        //     { canvas: [ { type: 'rect', x: 170, y: 32, w: pageSize.width - 170, h: 40 } ] }
+        //   ]
+        // },
+        // footer: function(currentPage, pageCount) { return currentPage.toString() + ' of ' + pageCount; }
+    }
+
+    console.log(docDefinition);
+
+    if (mode === 'show') {
+      pdfMake.createPdf(docDefinition).open({}, window.open('', '_blank') );
+    }
+    else if (mode == 'print') {
+      pdfMake.createPdf(docDefinition).print();
+    }
 
 }
