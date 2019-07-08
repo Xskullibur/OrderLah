4329f6f85c5df0f9eae41162bbcb2f5968d6850c
lee hsien xiang <leehsienx@gmail.com>
2019-07-08 20:19:25 +0800

    minor changes

--- server/routes/customer/customerRoutes.js
+++ server/routes/customer/customerRoutes.js
@@ -286,79 +286,13 @@ paypal.configure({
 
 //hsien xiang's route - done by hsienxiang and ziheng
 router.get('/payment', auth_login.auth, (req, res) => {
-
-    // var create_payment_json = {  //test data
-    //     "intent": "sale",
-    //     "payer": {
-    //         "payment_method": "paypal"
-    //     },
-    //     "redirect_urls": {
-    //         "return_url": "http://localhost:3000/paymentSuccess",
-    //         "cancel_url": "http://localhost:3000/paymentCancel"
-    //     },
-    //     "transactions": [{
-    //         "item_list": {
-    //             "items": [{
-    //                 "name": "food",
-    //                 "sku": "001",
-    //                 "price": "4.50",
-    //                 "currency": "SGD",
-    //                 "quantity": 1
-    //             }]
-    //         },
-    //         "amount": {
-    //             "currency": "SGD",
-    //             "total": "1.00"
-    //         },
-    //         "description": "This is the payment description."
-    //     }]
-    // }
-
-    // paypal.payment.create(create_payment_json, function (error, payment) {
-    //     if (error) {
-    //         throw error;
-    //     } else {
-    //         console.log(payment);
-    //         // for(let i = 0; i < payment.links.length; i++){
-    //         //     if(payment.links[i].rel === 'approval_url'){
-    //         //         res.redirect(payment.links[i].href)
-    //         //     }
-    //         // }
-    //     }
-    // })
+   
     var totalAmount = 3
     res.render('payment', {size: MenuItem.count(), totalAmount: totalAmount})
+    
 })
 
-// router.get('/paymentSuccess', (req, res) =>{
-//     const payerID = req.query.PayerID
-//     const paymentId = req.query.paymentId
 
-//     const execute_payment_json = {
-//         "payer_id": payerId,
-//         "transactions": [{
-//             "amount": {
-//                 "currency": "SGD",
-//                 "total": "4.50"
-//             }
-//         }]
-//     }
-
-//     paypal.payment.execute(paymentId, execute_payment_json, function (error, payment) {
-//         if (error) {
-//             console.log(error.response);
-//             throw error;
-//         } else {
-//             console.log(JSON.stringify(payment));
-//             res.send('Success');
-//         }
-//     });
-    
-// })
-
-// router.get('paymentCancel', (req, res) =>{
-//     res.send('cancel')
-// })
 
 
 module.exports = router
