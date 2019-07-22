63735eaba24c8cf751d68a36a19ac0d2bccadf13
lee hsien xiang <leehsienx@gmail.com>
2019-07-22 12:14:23 +0800

    admin alert for validation not showing

--- server/routes/administrator/adminRoutes.js
+++ server/routes/administrator/adminRoutes.js
@@ -32,6 +32,7 @@ const bcrypt = require('bcrypt')
 const saltRounds = 10
 
 var displayAlert = []
+var errorAlert = []
 
 const op = Sequelize.Op
 
@@ -51,8 +52,8 @@ function toCap(str) {
     return splitStr.join(' ')
 }
 
-function checkUnique(theName){
-    return MenuItem.count({where: {itemName: theName}}).then(count =>{
+function checkUniqueStall(theName){
+    return Stall.count({where: {stallName: theName}}).then(count =>{
         if(count !== 0){
             return false
         }
@@ -60,18 +61,47 @@ function checkUnique(theName){
     })
 }
 
+function checkUniqueEmail(theEmail){
+    return User.count({where: {email: theEmail}}).then(count =>{
+        if(count !== 0){
+            return false
+        }
+        return true
+    })
+}
+
+function checkUniqueUsername(theName){
+    return User.count({where: {username: theName}}).then(count =>{
+        if(count !== 0){
+            return false
+        }
+        return true
+    })
+}
+
+function checkUniquePhone(theNumber){
+    return User.count({where: {phone: theNumber}}).then(count =>{
+        if(count !== 0){
+            return false
+        }
+        return true
+    })
+}
 
 router.get('/adminPanel', auth_login.authAdmin, (req, res) =>{
     User.findAll({where: {role: "Stallowner"}}).then((stallowner) =>{
         res.render('admin', {
             displayStallowner: stallowner,
-            displayAlert: displayAlert
+            displayAlert: displayAlert,
+            errorAlert: errorAlert
         })
         displayAlert = []
+        errorAlert = []
     })
 })
 
-router.post('/submitStall', auth_login.authAdmin, (req, res) =>{
+router.post('/submitStall', auth_login.authAdmin, async (req, res) =>{
+    errorAlert = []
     var passGen = generator.generate({
         length: 15,
         numbers: true
@@ -88,39 +118,68 @@ router.post('/submitStall', auth_login.authAdmin, (req, res) =>{
     const stallName = toCap(req.body.stallName.replace(/(^\s*)|(\s*$)/gi, ""). replace(/[ ]{2,}/gi, " ").replace(/\n +/, "\n"))
     const description = req.body.description.replace(/(^\s*)|(\s*$)/gi, ""). replace(/[ ]{2,}/gi, " ").replace(/\n +/, "\n")
 
-    User.create({
-        username, firstName, lastName, email, birthday, password, phone, role
-    }).then(function(){
-        const emailcheck = req.body.email
-        User.findOne({ where: {email: emailcheck}}).then(user => {
+    await checkUniqueUsername(username).then(isUnique => {
+        if(!isUnique){
+            errorAlert.push('The username: ' + username + ' is already taken')
+        }
+    })
 
-            userId = user.id
+    await checkUniqueEmail(email).then(isUnique =>{
+        if(!isUnique){
+            errorAlert.push('The email: ' + email + ' is already used')
+        }
+    })
 
-            Stall.create({
-                userId, stallName, description
-            }).then(function(){  
-                var mailOptions = {
-                    from: 'Orderlah',
-                    to: email,
-                    subject: 'Account creation notice',
-                    text: 'Hi your username is ' + email + ' and password is ' + password
-                }
-            
-                transporter.sendMail(mailOptions, function (error, info) {
-                    if (error) {
-                        console.log(error);
-                    } else {
-                        console.log('Email sent: ' + info.response);
+    await checkUniquePhone(phone).then(isUnique => {
+        if(!isUnique){
+            errorAlert.push('The phone number: ' + phone + ' is already taken')
+        }
+    })
+
+    await checkUniqueStall(stallName).then(isUnique => {
+        if(!isUnique){
+            errorAlert.push('The stall name: ' + stallName + ' is already taken')
+        }
+    })
+
+    if(errorAlert.length > 0){
+        res.redirect('/admin/adminPanel')
+    }else{
+        User.create({
+            username, firstName, lastName, email, birthday, password, phone, role
+        }).then(function(){
+            const emailcheck = req.body.email
+            User.findOne({ where: {email: emailcheck}}).then(user => {
+    
+                userId = user.id
+    
+                Stall.create({
+                    userId, stallName, description
+                }).then(function(){  
+                    var mailOptions = {
+                        from: 'Orderlah',
+                        to: email,
+                        subject: 'Account creation notice',
+                        text: 'Hi your username is ' + email + ' and password is ' + password
                     }
-                })   
-            })
-
-            //res.render('./successErrorPages/createStallSuccess')
-            displayAlert.push("successfully added stall!")
-            res.redirect('/admin/adminPanel')
+                
+                    transporter.sendMail(mailOptions, function (error, info) {
+                        if (error) {
+                            console.log(error);
+                        } else {
+                            console.log('Email sent: ' + info.response);
+                        }
+                    })   
+                })
+    
+                //res.render('./successErrorPages/createStallSuccess')
+                displayAlert.push("successfully added stall!")
+                res.redirect('/admin/adminPanel')
+            }).catch(err => console.log(err))
+    
         }).catch(err => console.log(err))
-
-    }).catch(err => console.log(err))
+    }
+   
 })
 
 router.post('/lockAccount', auth_login.authAdmin, (req, res) =>{
