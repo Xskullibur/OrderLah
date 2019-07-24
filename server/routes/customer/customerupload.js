d88b8078feeaaa8eae429525e8a7df5d5ba3f8f3
Bryan <182869N@mymail.nyp.edu.sg>
2019-07-24 14:37:44 +0800

    minor changes

--- server/routes/customer/customerupload.js
+++ server/routes/customer/customerupload.js
@@ -1,13 +1,15 @@
 const multer = require('multer');
 const path = require('path');
 
+const uuidv4 = require('uuid/v4');
+
 var storage = multer.diskStorage({
     destination: function (req, file, cb){
         cb(null, process.cwd() + '/public/reviewimages')
         //cb(null, './public/reviewimages')
     },
     filename: function(req, file, cb){
-        cb(null, Date.now() + ".jpeg")
+        cb(null, uuidv4() + ".jpeg")
     }
 })
 
