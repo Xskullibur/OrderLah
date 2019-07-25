f3f79e89d388ac64a2650796e3689f4b0b692988
Peh Zi Heng <pehziheng@gmail.com>
2019-07-25 21:10:24 +0800

    Revert "Added initial push notifications changes"
    This reverts commit 5d84876a5341618a23edfad1a819acb5748e2ca8.

--- server/utils/stallowner/order.js
+++ server/utils/stallowner/order.js
@@ -6,7 +6,6 @@ const OrderItem = globalHandle.get('orderItem')
 const Order = globalHandle.get('order')
 const User = globalHandle.get('user')
 const Stall = globalHandle.get('stall')
-const MenuItem = globalHandle.get('menuItem')
 
 //Secure Random 
 const uuidv4 = require('uuid/v4')
@@ -74,28 +73,11 @@ module.exports = {
      * @param {number} userId - User id of all the orders
      * @return {Promise}
      */
-    getOrdersByUserId: function(userId){
+    getOrderByUserId: function(userId){
         return Order.findAll({
             where: {
                 userId
             }
-        })
-    },
-
-    /**
-     * Get all user orders (includes MenuItem) 
-     * @param {number} userId - User id of all the orders
-     * @return {Promise}
-     */
-    getOrdersWithMenuItemsByUserId: function(userId){
-        return Order.findAll({
-            where: {
-                userId
-            },
-            order: Sequelize.col('orderTiming'),
-            include: [{
-                model: MenuItem
-            }]
         })
     },
 
