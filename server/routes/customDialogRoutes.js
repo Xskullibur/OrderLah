3762055c4e1cd2e56dd8326c546e47e6f205f69f
Peh Zi Heng <pehziheng@gmail.com>
2019-07-29 22:31:44 +0800

    Added auth for custom dialog

--- server/routes/customDialogRoutes.js
+++ server/routes/customDialogRoutes.js
@@ -6,6 +6,11 @@ const express = require('express')
 //Create router
 const router = express.Router()
 
+//Login authentication middleware
+const auth_login = require('../libs/auth_login')
+
+router.use(auth_login.auth)
+
 /**
  * GET 'sample'
  * Send sample dialog html
